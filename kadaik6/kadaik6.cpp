ed0f12b20855b15f0ab8c580ec9d0c435ec54a83
ao0727 <qingduj@gmail.com>
2023-11-21 07:38:31 +0900

    プロジェクト ファイルを追加します。

---
+++ kadaik6/kadaik6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main()
+{
+   
+    srand(time(NULL));
+
+    int count = 100;
+
+    while (count > 0) {
+        int randomNumber = rand() % 2;// 0か1の乱数を生成し、それを1または-1に変換する
+        randomNumber = randomNumber + (randomNumber - 1);
+        cout << randomNumber << " ";
+        count--;
+
+    }
+
+}
+
